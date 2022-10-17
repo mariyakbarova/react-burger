eaf0d54ddc0f49922313219b503d2aaae17160c5
Mariya Akbarova <nailmaru@yandex.ru>
2022-10-17 21:31:03 +0300

    a

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/app/app.jsx';
+import App from './components/app1/app1.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
