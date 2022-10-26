8013c5a281df11d6a32d2920cf0f9b4052cb370a
Mariya Akbarova <nailmaru@yandex.ru>
2022-10-26 20:26:06 +0300

    ошибка в scroll

--- src/components/ingredients-set/ingredients-set.jsx
+++ src/components/ingredients-set/ingredients-set.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import { ingredientType } from '../../utils/components-prop-types.js';
 import IngredientCard from '../ingredient-card/ingredient-card.jsx';
 
-export default function IngredientsSet({ ingredients, type, onClick, sectionRef }) {
+export default function IngredientsSet({ ingredients, type, onClick, sectionRef, title }) {
   return (
     <section className='pb-10' ref={sectionRef}>
+      <h1 className='text text_type_main-medium mb-6'>{title}</h1>
       <ul className={styles.set}>
         {ingredients
           .filter((ingredient) => ingredient.type === type)
@@ -15,7 +16,8 @@ export default function IngredientsSet({ ingredients, type, onClick, sectionRef 
               ingredient={ingredient}
               onClick={onClick}
             />
-          ))}
+          ))
+          }
       </ul>
     </section>
   )
